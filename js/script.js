7ef982f92f3c45de81a378a5a65ccad46f056205
Ali <ali.nezamzadeh@gmail.com>
2024-06-23 17:36:35 +0330

    minor fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-// Show the loader
 document.getElementById("loader").style.display = "block";
 
-// Set a timer for 2 seconds
+
 setTimeout(function() {
-  // Hide the loader
+
   document.getElementById("loader").style.display = "none";
 }, 2500);
