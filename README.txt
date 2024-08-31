34d217c0cdac4f0cf34576ba609015eb01040881
alinz76 <ali.nezamzadeh@gmail.com>
2024-09-01 02:31:12 +0330

    first commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+# My Personal Website
+https://alinz76.github.io/
---
+++ assets/js/main2.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.section');
+    let delay = 0;
+
+    sections.forEach(section => {
+        setTimeout(() => {
+            section.classList.add('visible');
+        }, delay);
+        delay += 500; // Delay between each section appearing
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.section');
+    const options = {
+        root: null, // Use the viewport as the root
+        threshold: 0.1, // Trigger when 10% of the section is visible
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Stop observing once it's visible
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
